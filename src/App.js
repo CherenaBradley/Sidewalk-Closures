d373ed518ff03433c50c5523325b553a1907d01c
CherenaBradley <Cherenanbradley@gmail.com>
2020-07-26 19:21:53 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      //Anything in the state
+    };
+    //any functions
+  }
+//functions
+render() {
+
+  return (
+    <div className="app_container">
+        <p>Does this work</p>
+    </div>
+  )
+}
+
+}
+export default App;
